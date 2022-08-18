67a8e58a8f1b7df14c888bbda58b077176b647ad
Jonathan Smith <jpsmithuga@gmail.com>
2022-08-18 15:16:31 -0400

    Cleaned code

--- smith_response_LancetID_clean.R
+++ smith_response_LancetID_clean.R
@@ -1,121 +1,120 @@
-##################################
+######################################
 # Author: Jonathan Smith, PhD, MPH
-##################################
+# Email: jonathan.p.smith@yale.edu
+######################################
+# Clear global environment and graphics, if desired
+  # rm(list = ls())
+  # dev.off()
 
-# rm(list = ls())
-# dev.off()
+#' - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#' Global Parameters 
+#' - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#' Default figures are black and white (FALSE), but
+#' setting global option for color figures
 
-# Global Parameters
-R <- 3
-# Try alternate R values
-  # R <- 2
-  # R <- 1
-  # R <- 0.5
-k <- c(0.1, 0.5, 2, 100000)
+color_figures <- FALSE # If color figures are preferred, set to TRUE
+colors <- ggsci::pal_lancet(palette = c("lanonc"), alpha = 0.9)(9)
+
+#' R values from:
+#'   3.0 from Schneider and Eichner
+#'   0.36 from Blumberg and Lloyd-Smith
+R <- c(3.0, 0.36)
+
+#' k values are arbitrary
+#'   Note: k = 1000000 approximates
+#'   infinity (i.e. Poisson distribution/
+#'   uniform transmission)
+k <- c(0.10, 0.50, 2.00, 1000000)
 
 ###############################
 # Figure 1A
 ###############################
 
-# function to calculate proportion of infection (see supplementary document)
-propinfection <- function(R, k, prop){
-  xp <- qgamma(1 - prop, shape = k, rate = k/R) 
-  tp <- 1 - pgamma(xp, shape = k+1, rate = k/R) 
-  return(tp) 
+#' Function to calculate proportion of infection (see supplementary document)
+#'   @param R Negative Binomial R value
+#'   @param k Negative Binomial k value
+#'   @param p Proportion of infectious cases responsible for transmission
+
+propinfection <- function(R, k, prop) {
+  xp <- qgamma(1 - prop, shape = k, rate = k / R)
+  tp <- 1 - pgamma(xp, shape = k + 1, rate = k / R)
+  return(tp)
 }
 
-xx <- seq(0, 1, 0.001) # proportions 0 to 1 by 0.001
-#colors <- ggsci::pal_lancet(palette = c("lanonc"), alpha = 0.9)(9)
+# Set proportion driving secondary transmission, from 0 to 1 by 0.001
+xx <- seq(0, 1, 0.001) 
 
-plotdata <- cbind(xx, 
-                  propinfection(R = 3, k = k[1], xx),
-                  propinfection(R = 3, k = k[2], xx),
-                  propinfection(R = 3, k = k[3], xx),
-                  propinfection(R = 3, k = k[4], xx))
+#' Calculate proportion of expected transmission 
+#' for a given proportion of infectious cases
+plotdata <- sapply(seq_along(k), 
+                   function(x) propinfection(R = R[1], k = k[x], xx))
 
-plot(plotdata[,1], plotdata[,2], type = "l", bty = "n",
+# Plot Figure, Panel A
+plot(range(0, 1), range(0, 1), type = "n", bty = "n",
      xlab = "Proportion of Infectious Cases",
-     ylab = "Expected Proportion of Secondary Cases",
-     lwd = 2, lty = 4)
-  lines(plotdata[,1], plotdata[,3], lty = 3, lwd = 2)
-  lines(plotdata[,1], plotdata[,4], lty = 2, lwd = 2)
-  lines(plotdata[,1], plotdata[,5], lty = 1, lwd = 2)
+     ylab = "Expected Proportion of Secondary Cases")
+if(color_figures) {
+  sapply(seq_along(k), 
+         function(x) lines(xx, plotdata[, x], lwd = 2, col = colors[x], lty = 1))
   legend(x = 0.6, y = 0.35, c("k = 0.1", "k = 0.5", "k = 2.0", "No Variation"), 
+         lty = 1, bty = "n", lwd = 2, col = colors[1:4])
+  } else {
+  sapply(seq_along(k), 
+         function(x) lines(xx, plotdata[, x], lwd = 2, col = 1, lty = c(4:1)[x]))
+  legend(x = 0.6, y = 0.35, c("k = 0.1", "k = 0.5", "k = 2.0", "No Variation"),
          lty = 4:1, bty = "n", lwd = 2)
-  mtext(bquote(R[0]~"= 3.0"), adj = 1)
-  mtext("A)", adj = -0.1)
+}
+# mtext(bquote(R[0] ~ "= 3.0"), adj = 1)
+# mtext("A)", adj = -0.1)
+
 
 ###############################
-# B
+# Figure 1B
 ###############################
-# Function to calculate the probability of an outbreak of size Y
-outbreakprob <- function(y, R0, k){
-    if(y == 1){
-      1 / (1 + R0 / k)^k
-    }
-    else {
-      a <- 0
-      for(j in 0:(y - 2)){	
-        a <- a + log(j / k + y)
-      }
-      ll <- a - lfactorial(y) + log(((k / (R0 + k))^(k * y))*((R0 * k/(R0 + k))^(y - 1)))
-      exp(ll)
-    }
+
+#' Function to calculate the probability of an outbreak of size Y
+#'   @param y Final outbreak size
+#'   @param n Number of index cases (default to 1)
+#'   @param R Negative Binomial R value
+#'   @param k Negative Binomial k value
+outbreakprob <- function(y, n = 1, R, k) {
+  logp_yn <- log(n) - log(y) + 
+    lgamma(k * y + y - n) - (lgamma(k * y) + lgamma(y - n + 1)) + 
+    (y - n) * log(R / k) - (k * y + y - n) * log(1 + R / k)
+  return(exp(logp_yn))
 }
-  
-maxoutbreak <- 100
-outbrk_prob1 <- outbrk_prob2 <- outbrk_prob3 <- outbrk_prob4 <- c()
-for (i in 1:maxoutbreak){
-  outbrk_prob1[i] <- outbreakprob(y = i, R0 = R, k = k[1])
-  outbrk_prob2[i] <- outbreakprob(i, R, k[2])
-  outbrk_prob3[i] <- outbreakprob(i, R, k[3])
-  outbrk_prob4[i] <- outbreakprob(i, R, k[4])
-}
+
+# Set figure parameters
+maxoutbreak <- 100 # Plot to size 100
+pchh <- c(2, 3, 8, 19) # point selection if if B&W
+
+# Calculate outbreak probabilities
+outbrk_prob <- sapply(seq_along(k), 
+                      function(x) outbreakprob(y = 1:maxoutbreak, R = R[2], k = k[x]))
+
+# Setup plot to max cluster size from y = 1 to max size by 1
 maxrange <- seq(1, maxoutbreak, 1)
 
-plot(log(range(1, maxoutbreak)), range(-11,0), type = 'n', xlab = '', ylab = '', axes = F)
-  points(log(maxrange), log(outbrk_prob1), col = 1, pch=2, cex=1)
-  points(log(maxrange), log(outbrk_prob2), col = 1, pch=3, cex=1)
-  points(log(maxrange), log(outbrk_prob3), col = 1, pch=8, cex=1)
-  points(log(maxrange), log(outbrk_prob4), col = 1, pch=19, cex=1)
-  axis(side=1, at=log(c(1, 5, seq(10, maxoutbreak + 10, b = 10))), c(1, 5, seq(10, maxoutbreak + 10, b = 10)))
-  axis(side=2, at=log(c(1/100000, 1/10000, 1/1000, 1/100, 1/10, 1/2, 1)), c(1/100000, 1/10000, 1/1000, 1/100, 1/10, 1/2, 1))
-  mtext(side=1, 'Final Outbreak Size', padj = 4)
-  mtext(side=2, 'Probability', padj = -4)
+## Create plot
+plot(log(range(1, maxoutbreak)), range(-11,0), 
+     type = 'n', xlab = '', ylab = '', axes = FALSE)
+axis(side = 1, at = log(c(1, 5, seq(10, maxoutbreak + 10, b = 10))), 
+     label = c(1, 5, seq(10, maxoutbreak + 10, b = 10)))
+axis(side = 2, at = log(c(1/100000, 1/10000, 1/1000, 1/100, 1/10, 1/2, 1)), 
+     label = c(1/100000, 1/10000, 1/1000, 1/100, 1/10, 1/2, 1))
+mtext(side = 1, 'Final Outbreak Size', padj = 4)
+mtext(side = 2, 'Probability', padj = -4)
+if(color_figures){
+  sapply(seq_along(k), 
+         function(x) points(log(maxrange), log(outbrk_prob[,x]), 
+                            col = colors[x], pch = 19, cex = 1))
   legend('topright', c("k = 0.1", "k = 0.5", "k = 2.0", "No Variation"), 
-         pch=c(2,3,8,19), cex=1, bty="n")
-  mtext(bquote(R[0]~"= 3.0"), adj = 1)
-  mtext("B)", adj = -0.1)
-
-
-
-
-##### Combined Figure
-par(mfrow = c(1,2))
-# A
-plot(plotdata[,1], plotdata[,2], type = "l", bty = "n",
-     xlab = "Proportion of Infectious Cases",
-     ylab = "Expected Proportion of Secondary Cases",
-     lwd = 2, lty = 4)
-  lines(plotdata[,1], plotdata[,3], lty = 3, lwd = 2)
-  lines(plotdata[,1], plotdata[,4], lty = 2, lwd = 2)
-  lines(plotdata[,1], plotdata[,5], lty = 1, lwd = 2)
-  legend(x = 0.6, y = 0.35, c("k = 0.1", "k = 0.5", "k = 2.0", "No Variation"), 
-         lty = 4:1, bty = "n", lwd = 2)
-  mtext(bquote(R[0]~"= 3.0"), adj = 1)
-  mtext("A)", adj = -0.1, padj = -2)
-
-# B
-plot(log(range(1, maxoutbreak)), range(-11,0), type = 'n', xlab = '', ylab = '', axes = F)
-  points(log(maxrange), log(outbrk_prob1), col = 1, pch=2, cex=1)
-  points(log(maxrange), log(outbrk_prob2), col = 1, pch=3, cex=1)
-  points(log(maxrange), log(outbrk_prob3), col = 1, pch=8, cex=1)
-  points(log(maxrange), log(outbrk_prob4), col = 1, pch=19, cex=1)
-  axis(side = 1, at = log(c(1, 5, seq(10, maxoutbreak + 10, b = 10))), c(1, 5, seq(10, maxoutbreak + 10, b = 10)))
-  axis(side = 2, at = log(c(1/100000, 1/10000, 1/1000, 1/100, 1/10, 1/2, 1)), c(1/100000, 1/10000, 1/1000, 1/100, 1/10, 1/2, 1))
-  mtext(side = 1, 'Final Outbreak Size, Y', padj=4)
-  mtext(side = 2, 'Probability', padj=-4)
+         pch = 19, col = colors[1:4], cex = 1, bty = "n")
+} else {
+  sapply(seq_along(k), function(x) points(log(maxrange), log(outbrk_prob[,x]), 
+                                          col = 1, pch = pchh[x], cex = 1))
   legend('topright', c("k = 0.1", "k = 0.5", "k = 2.0", "No Variation"), 
-         pch=c(2,3,8,19), cex=1, bty="n")
-  mtext(bquote(R[0]~"= 3.0"), adj = 1)
-  mtext("B)", adj = -0.1, padj = -2)
+         pch = c(2, 3, 8, 19), cex = 1, bty = "n")
+}
+#mtext(bquote(R[0] ~ "= 0.36"), adj = 1)
+#mtext("B)", adj = -0.1)
